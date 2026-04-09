5108e2649e08b0e27e765cf2c2b10c366f68b2ce
Ayush Mishra <ayushmishra.5a@gmail.com>
2026-04-09 11:15:05 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Landing } from './pages';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        {/* Add more routes here */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
